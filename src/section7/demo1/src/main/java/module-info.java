9c34a3b4b69e0b9daa65a178ca2a9d7b95648598
Muhammet İkbal Yakupoğlu <ikbal.yakupoglu@ogr.sakarya.edu.tr>
2024-09-01 12:09:15 +0300

    java udemy multithreading kurs demo (7)

---
+++ src/section7/demo1/src/main/java/module-info.java
@@ -0,0 +1,8 @@
+//module org.nakoual.demo1 {
+//    requires javafx.controls;
+//    requires javafx.fxml;
+//
+//
+//    opens org.nakoual.demo1 to javafx.fxml;
+//    exports org.nakoual.demo1;
+//}
